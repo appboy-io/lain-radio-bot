1638857f6c4f196232a5347e9dd2ea593c56a6db
Bryan Riley <brileydeveloper@gmail.com>
2021-02-15 11:26:37 -0500

    Adding sudo to docker calls to run

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,9 +5,9 @@ sudo apt install docker -y
 sudo apt install docker-compose -y
 
 sudo systemctl enable docker
- 
+
 sudo systemctl start docker
 
-docker-compose up --build -d
+sudo docker-compose up --build -d
 
 
